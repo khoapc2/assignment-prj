6c8b8be516f6d85cac9cfd394f90619270609197
longdaungo <zedvidieu@gmail.com>
2021-10-23 20:03:29 +0700

    -make a frame to my team

---
+++ PitchBooking/PitchBooking.Business/IServices/IPitch.cs
@@ -0,0 +1,14 @@
+﻿using PitchBooking.Business.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PitchBooking.Business.IServices
+{
+    public interface IPitch
+    {
+        Task<PitchModel> GetPitchModel(PitchModel filter);
+    }
+}
---
+++ PitchBooking/PitchBooking.Business/ViewModel/PitchModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PitchBooking.Business.ViewModel
+{
+    public class PitchModel
+    {
+        public int Id { get; set; }
+        public int? PitchOwnerId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string ImgPath { get; set; }
+        public string Time { get; set; }
+        public int? Rates { get; set; }
+        public int? Status { get; set; }
+    }
+}
