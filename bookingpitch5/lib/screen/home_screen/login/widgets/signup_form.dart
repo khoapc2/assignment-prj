c3f8f076acdcf6bedcb3408bc49a7fb44ecf4be1
quoclinh1201 <quoclinh1212000@gmail.com>
2021-10-25 03:55:07 +0700

    implement sign up, 99,99% done

--- bookingpitch5/lib/screen/home_screen/login/widgets/signup_form.dart
+++ bookingpitch5/lib/screen/home_screen/login/widgets/signup_form.dart
@@ -1,5 +1,14 @@
+import 'package:bookingpitch5/view_models/sign_up_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:bookingpitch5/screen/home_screen/login/theme.dart';
+
+TextEditingController edtFullname = new TextEditingController();
+TextEditingController edtEmail = new TextEditingController();
+TextEditingController edtPhone = new TextEditingController();
+TextEditingController edtAddress = new TextEditingController();
+TextEditingController edtUsername = new TextEditingController();
+TextEditingController edtPassword = new TextEditingController();
+TextEditingController edtConfirmPassword = new TextEditingController();
 
 class SignUpForm extends StatefulWidget {
   @override
@@ -12,20 +21,39 @@ class _SignUpFormState extends State<SignUpForm> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        buildInputForm('First Name', false),
-        buildInputForm('Last Name', false),
-        buildInputForm('Email', false),
-        buildInputForm('Phone', false),
-        buildInputForm('Password', true),
-        buildInputForm('Confirm Password', true),
+        Padding(
+          padding: kDefaultPadding,
+          child: Column(children: [
+            buildInputForm('Full Name', false, edtFullname),
+            buildInputForm('Email', false, edtEmail),
+            buildInputForm('Phone', false, edtPhone),
+            buildInputForm('Address', false, edtAddress),
+            buildInputForm('Username', false, edtUsername),
+            buildInputForm('Password', true, edtPassword),
+            buildInputForm('Confirm Password', true, edtConfirmPassword),
+          ]),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: kDefaultPadding,
+          child: PrimaryButton('Sign Up'),
+        ),
       ],
     );
   }
 
-  Padding buildInputForm(String hint, bool pass) {
+  Padding buildInputForm(
+      String hint, bool pass, TextEditingController editingController) {
     return Padding(
         padding: EdgeInsets.symmetric(vertical: 5),
         child: TextFormField(
+          controller: editingController,
+          keyboardType: hint == 'Phone' ? TextInputType.phone : null,
           obscureText: pass ? _isObscure : false,
           decoration: InputDecoration(
             hintText: hint,
@@ -51,5 +79,100 @@ class _SignUpFormState extends State<SignUpForm> {
                 : null,
           ),
         ));
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  final String buttonText;
+  var fullname, email, phone, address, username, password, confirmPassword;
+  PrimaryButton(this.buttonText);
+
+  @override
+  State<PrimaryButton> createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  String error = '';
+  bool isRegister = false;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.08,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () async => {
+          error = SignUpViewModel.validateSignUp(
+              edtFullname.text,
+              edtEmail.text,
+              edtPhone.text,
+              edtAddress.text,
+              edtUsername.text,
+              edtPassword.text,
+              edtConfirmPassword.text),
+          if (error.length > 0)
+            {
+              showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Đăng ký thất bại'),
+                  content: Text(error),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, 'OK'),
+                      child: const Text('OK'),
+                    ),
+                  ],
+                ),
+              ),
+            }
+          else
+            {
+              isRegister = await SignUpViewModel.registerAccount(
+                  edtFullname.text,
+                  edtEmail.text,
+                  edtPhone.text,
+                  edtAddress.text,
+                  edtUsername.text,
+                  edtPassword.text),
+                  if(isRegister) {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Đăng ký thành công'),
+                        content: Text('Đăng ký thành công!!!'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    )
+                  } else {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('Đăng ký thất bại'),
+                        content: Text('Username đã tồn tại!'),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    )
+                  }
+            },
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+        ),
+        child: Text(
+          widget.buttonText,
+          style: textButton.copyWith(color: kWhiteColor),
+        ),
+      ),
+    );
   }
 }
---
+++ bookingpitch5/lib/view_models/sign_up_view_model.dart
@@ -0,0 +1,44 @@
+import 'package:bookingpitch5/api/user_account_services.dart';
+import 'package:bookingpitch5/models/sign_up_model.dart';
+
+class SignUpViewModel {
+  static String validateSignUp(
+      String fullname,
+      String email,
+      String phone,
+      String address,
+      String username,
+      String password,
+      String confirmPassword) {
+    var error = '';
+
+    if (fullname.length < 1 || fullname.length > 50)
+      error += 'Fullname is required and length <= 50\n';
+    if (email.length > 50) error += 'Emai length must <= 50\n';
+    if (phone.length != 10)
+      error += 'Phone is required and length must = 10 characters\n';
+    if (address.length > 50) error += 'Address length must <= 50\n';
+    if (username.length < 1 || username.length > 20)
+      error += 'Username is required and length <= 20\n';
+    if (password.length < 1 || password.length > 20)
+      error += 'Password is required and length <= 20\n';
+    if (confirmPassword.length < 1 || password.length > 20)
+      error += 'Confirm Password is required and length <= 20\n';
+    if (password != confirmPassword)
+      error += 'Password and Confirm Password is not matched!';
+    return error;
+  }
+
+  static Future<bool> registerAccount(
+    String fullname, 
+    String email,
+    String phone, 
+    String address, 
+    String username, 
+    String password) async {
+    UserAccountServices serives = new UserAccountServices();
+    var isRegister = await serives.SignUp(
+        new SignUpModel(fullname, email, phone, address, username, password));
+        return isRegister;
+  }
+}
