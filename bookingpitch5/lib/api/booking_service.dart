c3044599196a648cfc6feebc8b0a161ab67c00b4
quoclinh1201 <quoclinh1212000@gmail.com>
2021-10-26 17:24:17 +0700

    inplement cancel booking

--- bookingpitch5/lib/api/booking_service.dart
+++ bookingpitch5/lib/api/booking_service.dart
@@ -7,6 +7,7 @@ class BookingService {
   var url = 'https://104.215.186.78/api/v1/bookings'; // server
   HttpClient client = HttpClient();
 
+  //add token
   Future<List<BookingModel>> getListBookedByCustomerID(id) async {
     List<BookingModel> result = List.empty();
     client.badCertificateCallback =
@@ -21,11 +22,32 @@ class BookingService {
       //   List d = jsonDecode(data);
       //   result = d.map((e) => BookingModel.fromJson(e)).toList(); //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
       // });
-      List ds = jsonDecode(await response.transform(utf8.decoder).join());
-      result = ds.map((e) => BookingModel.fromJson(e)).toList();
+      List data = jsonDecode(await response.transform(utf8.decoder).join());
+      result = data.map((e) => BookingModel.fromJson(e)).toList();
       return result;
     } else {
       return result;
+    }
+  }
+
+  //add token
+  Future<bool> cancelBooking(int id, String reason) async {
+    var data = {
+      "id": id,
+      "cancel_reason": reason
+    };
+    client.badCertificateCallback =
+        ((X509Certificate cert, String host, int port) => true);
+    HttpClientRequest request =
+        await client.deleteUrl(Uri.parse(url + "/cancel"));
+    request.headers.set('content-type', 'application/json; charset=UTF-8');
+    request.add(utf8.encode(json.encode(data)));
+    HttpClientResponse response = await request.close();
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
--- bookingpitch5/lib/view_models/my_booking_view_model.dart
+++ bookingpitch5/lib/view_models/my_booking_view_model.dart
@@ -3,6 +3,13 @@ import 'package:bookingpitch5/api/booking_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MyBookingViewModel {
+  static List<String> reasonList = [
+    'Không đủ thành viên',
+    'Bận đột xuẩt',
+    'Thời tiết xấu',
+    'Lý do khác'
+  ];
+
   static Future<List<BookingModel>> getListBookedByCustomerID() async {
     final prefs = await SharedPreferences.getInstance();
     BookingService service = new BookingService();
@@ -10,5 +17,10 @@ class MyBookingViewModel {
     var id = 2; /////////////////////////////////////////
     var result = await service.getListBookedByCustomerID(id);
     return result;
+  }
+
+  static Future<bool> cancelBooking(int id, String reason) async {
+    BookingService service = new BookingService();
+    return service.cancelBooking(id, reason);
   }
 }
